c5c201d4eb40a737e7d312d0af349df0384541d5
Jack Tang <1190545+jackhftang@users.noreply.github.com>
2019-03-17 08:53:14 +0800

    UPT: use globals variables instead of shared

--- main.nim
+++ main.nim
@@ -1,12 +1,23 @@
 import jester, locks, strformat
 from math import `^`
 
-const size = 4 # size of array[size,int]
-const width = 9 # largest value of each int in array is 10^wdith 
+const size = 4
+const width = 
+  case sizeof(0):
+    of 4: 9
+    of 8: 18
+    else: 1
 
-proc increment(arr: ptr array[size, int]) = 
+var
+  countLock: Lock
+  # count {.guard: countLock.} : array[4,int] 
+  count: array[size,int] 
+
+initLock countLock
+
+proc increment(arr: var openArray[int]) = 
   var carry = 1
-  for i in 0..<size:
+  for i in 0..arr.high:
     arr[i] += carry 
     if arr[i] == 10^width:
       arr[i] = 0 
@@ -14,10 +25,10 @@ proc increment(arr: ptr array[size, int]) =
     else:
       carry = 0
 
-proc toString(arr: ptr array[size, int]): string = 
+proc toString(arr: var openArray[int]): string = 
   result = "0" 
   var padZero = false
-  for i in countDown(size-1,0):
+  for i in countDown(arr.high,0):
     let n = arr[i]
     if padZero: 
         result = result & fmt("{n:0" & $width & "}")
@@ -25,18 +36,12 @@ proc toString(arr: ptr array[size, int]): string =
       result = $n
       padZero = true   
 
-var
-  lock: Lock
-  count = createShared(array[size, int], 1)
-  
-initLock lock
-
-proc hit(): Future[string] {.async, gcsafe.} =
+proc hit(): Future[string] {.async.} =
   while true:
-    if tryAcquire(lock):
+    if tryAcquire(countLock):
       increment(count)
       result = toString(count)
-      release(lock)
+      release(countLock)
       break
     else:
       await sleepAsync(10)
@@ -44,3 +49,4 @@ proc hit(): Future[string] {.async, gcsafe.} =
 routes:
   get "/":
     resp Http200, [("Keep-Alive", "timeout=5, max=1000")], await hit()
+    
